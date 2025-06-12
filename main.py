522a6bc24367918ec90dd951281d9d5a243516f8
Razi Muhammed <razimuhammedkv@gmail.com>
2025-06-12 22:47:02 +0530

    Resize an image - Topic 2

--- main.py
+++ main.py
@@ -1,21 +1,50 @@
-                        #READ, DISPLAY, & SAVE AN IMAGE
+import cv2 as cv
+import numpy as np
+
+#RESİZE AN IMAGE
+
+img2 = cv.imread('zoo.jpg') 
+cv.imshow('original', img2)
+print(img2.shape) #(height, width, channel) tuple
+
+#Downscale with aspect ratio
+
+scale_percent = 50 #%50 of the original size
+width = int(img2.shape[1] * scale_percent / 100) 
+height = int(img2.shape[0] * scale_percent / 100)
+dim = (width, height) # dimension ple
+resized1 = cv.resize(img2, dim, interpolation = cv.INTER_AREA)
+cv.imshow('resized1', resized1) #resized image
+
+print(resized1.shape) 
+
+# #Upscale with aspect ratio
+# scale_percent = 150    #%150 of the original size
+# width = int(img2.shape[1] * scale_percent / 100) 
+# height = int(img2.shape[0] * scale_percent / 100)
+# dim = (width, height) 
+# resized2 = cv.resize(img2, dim, interpolation = cv.INTER_AREA)
+# cv.imshow('resized2', resized2) #resized image
+
+# print(resized2.shape)
 
 
-import cv2 as cv                          #importing the open cv library with alias cv
+# #change width only
+# width = int(img2.shape[1])
+# height = 150 
+# dim = (width, height) 
+# resized3 = cv.resize(img2, dim, interpolation = cv.INTER_AREA)
+# cv.imshow('resized3', resized3) #resized image
 
-img = cv.imread('parrot.jpg')             #reading the image
-#img1 = cv.imread('parrot.jpg',0)         #reading the image in grey scale
+# print(resized3.shape)
 
-cv.imshow('Parrot', img)                  #displaying the image
-#cv.imshow('Parrot-grey', img1)
+# #custom size
+# width = 300
+# height = 300
+# dim = (width, height)
+# resized4 = cv.resize(img2, dim, interpolation = cv.INTER_AREA)
+# cv.imshow('resized4', resized4) #resized image
 
 
-
-#cv.imwrite('grey-parrot.jpg', img1)      #save the image
-
-
-cv.waitKey(5000)                          #image will be displayed for 5 seconds
-
-#cv.waitKey(0)
-#cv.destroyAllWindows                     #image will disappear when any key pressed
-
+cv.waitKey(0)
+cv.destroyAllWindows()
