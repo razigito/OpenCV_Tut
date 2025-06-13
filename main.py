b204bf11d28bb28cb4af3bdc4a8a22a5ccba9b38
Razi Muhammed <razimuhammedkv@gmail.com>
2025-06-13 22:35:14 +0530

    Adding text to the image - Topic 5

--- main.py
+++ main.py
@@ -9,29 +9,8 @@ import matplotlib.pyplot as plt
 
 img=cv.imread('badminton.jpg')
 
-
-# # #1. draw a line
-# cv.line(img,(192,100),(276,163),(0,0,255),3)    #(192,100) is the starting point and (276,163) is the ending point
-#                                                 #(0,0,255) is the color of the line and 3 is the thickness of the line
-#                                                #OpenCV's cv2.line() only accepts pixel coordinates as tuples of integers. Floats are invalid because a pixel can’t be partially between two locations.
-# cv.imshow('line',img)
-
-# #2. draw a rectangle
-# cv.rectangle(img,(240,125),(325,200),(0,255,0),3) #(240,125) is the starting point and (325,200) is the ending point, diagonal points
-#                                                 #(0,255,0) is the color of the rectangle and 3 is the thickness of the rectangle
-# cv.imshow('rectangle',img)
-
-# #3. draw a circle
-# cv.circle(img,(280,160),50,(0,0,255),3) #(280,160) is the center of the circle and 50 is the radius of the circle
-# cv.imshow('circle',img)
-
-# #fill the circle
-# cv.circle(img,(280,160),50,(0,0,255),-1) #-1 , give -ve thickness to fill the circle
-# cv.imshow('filled circle',img)
-
-   
-
-
+cv.putText(img,'P V Sindhu',(250,100),cv.FONT_HERSHEY_DUPLEX,1,(0,255,255),2) #(250,100) is the starting point of the text
+cv.imshow('Text',img)
 
 
 
