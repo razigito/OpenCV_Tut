365b6e187ce3ea0a9cacb67e426c230e8af956cf
Razi Muhammed <razimuhammedkv@gmail.com>
2025-06-15 01:31:24 +0530

    crop an image - Topic 8

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
-import cv2 as cv  
-cap = cv.VideoCapture(0) #capture the video from the webcam,
-                        #you can change the 0 to the video file name
-                        #to capture the video from the video file
+import cv2 as cv
+import matplotlib.pyplot as plt
+# img = plt.imread('messi.jpg')
 
-while True:
-    
-    ret, frame = cap.read()
-    
-    
-    if not ret:
-        print("Error: Could not read frame")
-        break
-    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) #convert the frame to gray
-    hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    cv.putText(frame, 'Razi', (50, 100), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 250), 3)
-    cv.imshow('original', frame)    #show the original frame
-    cv.imshow('gray',gray_frame)    #show the gray frame
-    cv.imshow('hsv', hsv_frame)
+# plt.imshow(img)
+# plt.show()
 
-    
-  
-    if cv.waitKey(1) == ord('q'):
-        break
-# Release the webcam resource
-cap.release()
-# Close all OpenCV windows
+img = cv.imread('messi.jpg')      # Read the image
+cropped = img[10:168, 100:250]   # Crop: [y1:y2, x1:x2]
+cv.imshow("Original", img)
+cv.imshow("Cropped", cropped)     # Show cropped part
+cv.waitKey(0)
 cv.destroyAllWindows()
-
-
